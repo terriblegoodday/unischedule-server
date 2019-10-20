e929e81da0448b236927a0fe0eee172f593018d4
Eduard <terriblegoodday@icloud.com>
2019-10-20 23:22:17 +1000

    updated the `npm run dev` and `npm build` scripts

--- start.js
+++ start.js
@@ -1,3 +1,17 @@
-import { ApolloServer, gql } from 'apollo-server'
-import typeDefs from './typedefs'
+import { ApolloServer } from 'apollo-server'
+import { typeDefs } from './typedefs'
+import { resolvers } from './resolvers'
+import { Teacher, Group, Class } from './database'
 
+const server = new ApolloServer(
+  {
+    typeDefs,
+    resolvers
+  }
+)
+
+export function start() {
+    server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`)
+  })
+}
